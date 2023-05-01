7c73a66cae22191221202536ebf7ea56b1c45f15
nithinsikinam <nithinsikinam@gmail.com>
2023-05-01 09:56:53 +0530

    changes

--- auth/src/index.js
+++ auth/src/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hllo worlD');
-})
+  res.send('hllo wrlD');
+});
 
 app.listen(5000, (req, res) => {
   console.log('app started');
